5c1b1281b291a22a68aee391bda2ca314123376b
Jonathan Tran <jonathanmhtran@gmail.com>
2024-04-20 13:26:30 -0700

    Added skeleton Chat page

--- TravelBuddy/App.js
+++ TravelBuddy/App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, Button } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from "./components/Home";
+import Map from "./components/Map";
+import Chat from './components/Chat';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen
+          name='Home'
+          component={Home}
+        />
+        <Stack.Screen 
+          name='Map'
+          component={Map}  
+        />
+        <Stack.Screen 
+          name='Chat'
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ) 
+};
