cd41f7bafa6e1cc9f7dfcf4a94b2fabd62f5c418
Jonathan Tran <jonathanmhtran@gmail.com>
2024-04-20 13:26:09 -0700

    Added skeleton Chat page

---
+++ TravelBuddy/components/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Animated } from 'react-native';
+
+const Chat = ({navigation}) => {
+  return (
+    <View>
+        <Text>
+            Replace Text with your code :D
+        </Text>
+    </View>
+  )
+}
+
+export default Chat;
