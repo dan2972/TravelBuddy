f3b3260cf8296631281d9093faf74f933765fd85
Jonathan Tran <jonathanmhtran@gmail.com>
2024-04-20 13:28:29 -0700

    Added skeleton for Map

---
+++ TravelBuddy/components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const Map = () => {
+  return (
+    <View>
+        <Text>
+            Hello
+        </Text>
+    </View>
+  )
+}
+
+export default Map;
