39ba060f0fae200ed91811be009085f8c99a494a
Jonathan Tran <jonathanmhtran@gmail.com>
2024-04-20 14:36:04 -0700

    Implemented basic Google Maps View

--- TravelBuddy/components/Map.js
+++ TravelBuddy/components/Map.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const Map = () => {
   return (
-    <View>
-        <Text>
-            Hello
-        </Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE} />
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;
