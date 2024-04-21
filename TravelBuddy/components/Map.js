89591706ace3d697a2d424a272b88e36e7ce4d95
Jonathan Tran <jonathanmhtran@gmail.com>
2024-04-20 20:37:48 -0700

    Added POST request to Google Places API, changed marker icon to circles

--- TravelBuddy/components/Map.js
+++ TravelBuddy/components/Map.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, FlatList, Text, TouchableOpacity } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { View, TextInput, Button, Image, StyleSheet, FlatList, Text, TouchableOpacity, Alert, Pressable } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
+import { circle } from "../assets/circle.svg";
+import Svg, { Circle } from 'react-native-svg';
 
 const Map = () => {
   const [region, setRegion] = useState({
@@ -10,48 +12,138 @@ const Map = () => {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
-  const [marker, setMarker] = useState(null);
+  const [center, setCenter] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324
+  });
+  const [markers, setMarkers] = useState([]); // Use an array to manage multiple markers
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [genMarkers, setGenMarkers] = useState([]);
 
-  // Function to handle map press to update the marker position manually
+  // Function to handle map press to add a new marker
   const handleMapPress = (e) => {
-    setMarker({
+    const newMarker = {
       latitude: e.nativeEvent.coordinate.latitude,
-      longitude: e.nativeEvent.coordinate.longitude
-    });
+      longitude: e.nativeEvent.coordinate.longitude,
+      key: Math.random().toString() // Unique key for each marker
+    };
+    setMarkers([...markers, newMarker]);
   };
 
   // Function to search location using Google Places API
   const searchLocation = async (search) => {
-    const apiKey = process.env.EXPO_PUBLIC_MAPS_API_KEY; // Replace with your actual API key
-    console.log(apiKey);
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&key=${apiKey}`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(search)}&key=${process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}`;
 
     try {
       const response = await fetch(apiUrl);
       const json = await response.json();
-      console.log(json);
-      if (json.results.length > 0) {
-        setResults(json.results);
-      } else {
-        console.warn('No results found');
-        setResults([]);
-      }
+      setResults(json.results);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
   const selectLocation = (location) => {
-    const { lat, lng } = location.geometry.location;
+    const {lat, lng} = location.geometry.location;
     setRegion({
       latitude: lat,
       longitude: lng,
-      latitudeDelta: 0.02,
-      longitudeDelta: 0.02
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005
     });
   };
+
+  // Function to add a marker
+  const addMarker = (location) => {
+    const lat = location.latitude;
+    const lng = location.longitude;
+    const newMarker = {
+      latitude: lat,
+      longitude: lng,
+      key: `s_${Date.now()}` // Different prefix for search-based markers
+    }
+    console.log(markers);
+    setMarkers([...markers, newMarker]);
+  }
+
+  // Function to remove a marker
+  const removeMarker = (key) => {
+    setMarkers(markers.filter(marker => marker.key !== key));
+  };
+
+  const handleRegionChange = (newRegion) => {
+    const lat = newRegion.latitude;
+    const long = newRegion.longitude;
+    setCenter({latitude: lat, longitude: long});
+  }
+
+  
+  // Optional: Alert to confirm removal
+  const handleMarkerPress = (marker) => {
+    Alert.alert(
+      "Remove Marker",
+      "Do you want to remove this marker?",
+      [
+        { text: "Cancel", style: "cancel" },
+        { text: "OK", onPress: () => removeMarker(marker.key) }
+      ]
+    );
+  };
+
+  const generateLocations = async () => {
+    markers.map(async (marker) => {
+      const apiURL = `https://places.googleapis.com/v1/places:searchNearby`;
+      console.log(marker.longitude)
+
+      const formData = new FormData();
+
+      formData.append("includedTypes", ["hospital"]);
+      formData.append("locationRestriction", {
+        "circle": {
+          "center": {
+            "latitude": marker.latitude,
+            "longitude": marker.longitude
+          },
+          "radius": 10000.0
+        }
+      });
+
+      const formBody = {
+        "includedTypes": [
+          "restaurant"
+        ],
+        "maxResultCount": 10,
+        "locationRestriction": {
+          "circle": {
+            "center": {
+              "latitude": 37.7937,
+              "longitude": -122.3965
+            },
+            "radius": 500
+          }
+        }
+      };
+      formData.append("maxResultCount", 4);
+
+      try {
+        const response = await fetch(apiURL, {
+          method: 'POST',
+          body: JSON.stringify(formBody),
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Goog-Api-Key': process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+            'X-Goog-FieldMask': "places.displayName,places.location,places.formattedAddress"
+          }
+        });
+        const data = await response.json();
+        console.log(data);
+      }
+      catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -59,15 +151,39 @@ const Map = () => {
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         onPress={handleMapPress}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
         region={region}
+        onRegionChange={handleRegionChange}
       >
-         <Marker coordinate={region} />
+        {markers.map((marker, index) => (
+          <Marker
+            centerOffset={{ x: 0, y: 0}}
+            key={marker.key}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            onPress={() => handleMarkerPress(marker)}
+          >
+            <Text style={{opacity: 0}}>''</Text>
+            <Svg height={50} width={50}>
+              <Circle
+                cx={25}
+                cy={35}
+                r={15}
+                fill="rgba(255, 0, 0, 0.5)" // Customize fill color and opacity
+              />
+            </Svg>
+          </Marker>
+        ))}
+
+        <Marker 
+          style={styles.centerMarker}
+          pinColor="#ab39fe"
+          coordinate={center}
+        />
+
+        {/* {resourceMarkers.map((resourceMarker, index) => {
+          <Marker 
+            key={index}
+          />
+        })} */}
       </MapView>
       <TextInput
         style={styles.searchBox}
@@ -75,28 +191,47 @@ const Map = () => {
         onChangeText={setSearchTerm}
         placeholder="Search for a place"
       />
-      <Button title="Search" onPress={() => searchLocation(searchTerm)} />
-      <FlatList
-        data={results}
-        keyExtractor={(item) => item.place_id}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.listItem} onPress={() => selectLocation(item)}>
-            <Text style={styles.itemText}>{item.name} - {item.formatted_address}</Text>
-          </TouchableOpacity>
-        )}
-        style={styles.resultsList}
-      />
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={() => addMarker(center)}>
+          <Text style={styles.buttonText}>Add Marker</Text>
+        </Pressable>
+
+        <Pressable style={[styles.button, styles.searchButton]} title="Search" onPress={() => searchLocation(searchTerm)}>
+          <Text style={styles.buttonText}>Search</Text>
+        </Pressable>
+      </View>
+
+      <Pressable style={styles.button} onPress={() => generateLocations(markers)}>
+        <Text style={styles.buttonText}>Generate</Text>
+      </Pressable>
+      {searchTerm && results.length > 0 && (
+        <FlatList
+          data={results}
+          keyExtractor={(item) => item.place_id}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.listItem} onPress={() => selectLocation(item)}>
+              <Text style={styles.itemText}>{item.name} - {item.formatted_address}</Text>
+            </TouchableOpacity>
+          )}
+          style={styles.resultsList}
+        />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
   },
   map: {
     width: '100%',
-    height: '60%', // Adjust the map height to accommodate the list
+    height: '50%',
+  },
+  centerMarker: {
+    opacity: 0.5
   },
   searchBox: {
     position: 'absolute',
@@ -108,11 +243,32 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingHorizontal: 10,
   },
+  buttonContainer: {
+    display: 'flex',
+    width: "100%",
+    flexDirection: 'row',
+    justifyContent: "space-evenly",
+    marginTop: 5
+  },
+  button: {
+    backgroundColor: "#7abbff",
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginBottom: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 6
+  },
+  buttonText: {
+    fontSize: 20,
+  },
   resultsList: {
     position: 'absolute',
-    top: '70%', // Adjust as needed
+    top: '70%',
     width: '100%',
-    height: '30%', // Define how much space the list should take
+    height: '30%',
   },
   listItem: {
     backgroundColor: 'white',
